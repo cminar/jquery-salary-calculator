4e42142f240254f20ac1eb27c50aeaa3a4f45da9
cminar <colinminar@gmail.com>
2020-01-31 16:54:28 -0600

    Structure

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ console.log( 'in JS' );
 
 $( document ).ready( onReady );
 
+// Global
+let employees = [];
+
 function onReady(){
     console.log( 'in JQ' );
+    $( '#submitButton' ).on( 'click', addEmployee);
+    $( '.delete' ).on( 'click', deleteEntry );
 }
+
+// Adds employee to employeeArray
+function addEmployee(){
+    console.log( 'in addEmployee' );
+    // push employee object to DOM
+    // update DOM
+}
+
+// Deletes employee from Array and DOM
+function deleteEntry(){
+    console.log( 'in deleteEntry' );
+    // splice array
+    //update DOM
+}
+
+
+// make table??????
+// update DOM
+// calculate total and push to DOM
+// 
